b8c0b5bc262f5a3b60825d4450554581d0ff4614
herotran0504 <herotran0504@gmail.com>
2023-11-17 19:57:34 -0600

    - Add order confirmation page

--- src/pages/OrderConfirmationPage.js
+++ src/pages/OrderConfirmationPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import '../styles/order_confirmation.css'
 
 export const OrderConfirmationPage = () => {
+    const orderId = "11234556423146230";
     return (
-        <h1>Order confirmation page</h1>
-    )
+        <>
+            <main className="main-container">
+                <img className="image-wrapper" alt=""/>
+                <h2 className="title">Your order was successful</h2>
+                <p className="description">Thanks for your purchase!</p>
+                <div className="order-number">
+                    <span className="order-number-label">Your order number is #</span>
+                    <span className="order-number-value">{orderId}</span>
+                </div>
+                <p className="order-details">You’ll receive an email confirming your order details</p>
+                <a href="#" className="track-order">Track your order</a>
+                <a href="#" className="back-to-home">Back to home</a>
+            </main>
+        </>
+    );
 }
