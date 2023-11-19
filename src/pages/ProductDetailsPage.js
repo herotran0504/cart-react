f1b9785e744d4ee01b6ca831a5a8b981864cdc5c
samurai-vn <dungpx1501@gmail.com>
2023-11-19 02:21:04 -0600

    add header&footer

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -4,8 +4,10 @@ import {ReviewList} from "./components/ReviewList";
 import RatingStar from "./components/RatingStar";
 import ProductDetail from "./components/ProductDetail";
 import ReviewFrame from "./components/ReviewFrame";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export const ProductDetailsPage = (props) => {
+export const ProductDetailsPage = () => {
     const location = useLocation();
     const productItem = location.state.item;
     console.log(productItem);
@@ -17,16 +19,20 @@ export const ProductDetailsPage = (props) => {
         alert("Add to cart coming soon:)")
     }
     return (
-        <div className="product-detail-page">
-            <h1>Product details page</h1>
-            <div className="container">
-                <div className="product-detail-item">
-                    <ProductDetail productItem={productItem} onAddToCart={handleAddToCart}/>
-                </div>
-                <div className="product-detail-review">
-                    <ReviewFrame reviewList={productItem.reviewDTOList} onAddComment={handleAddComment}/>
+        <div>
+            <Header/>
+            <div className="product-detail-page">
+                <h1>Product details page</h1>
+                <div className="container">
+                    <div className="product-detail-item">
+                        <ProductDetail productItem={productItem} onAddToCart={handleAddToCart}/>
+                    </div>
+                    <div className="product-detail-review">
+                        <ReviewFrame reviewList={productItem.reviewDTOList} onAddComment={handleAddComment}/>
+                    </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     )
 }
